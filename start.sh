3af9391bebbc5a630710f5bd894d890782d9196c
Jannis R <mail@jannisr.de>
2021-09-07 21:10:03 +0200

    start.sh: set up trap first 🐛

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ set -o pipefail
 
 lib="$(dirname $(realpath $0))/lib"
 
+# kill child processes on exit
+# https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits/2173421#2173421
+trap 'exit_code=$?; kill -- $(jobs -p); exit $exit_code' SIGINT SIGTERM EXIT
+
 NODE_ENV=production node_modules/.bin/monitor-hafas \
 	$lib/hafas.js \
 	&
@@ -15,9 +19,5 @@ NODE_ENV=production node_modules/.bin/match-with-gtfs \
 
 NODE_ENV=production node_modules/.bin/serve-as-gtfs-rt \
 	&
-
-# kill child processes on exit
-# https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits/2173421#2173421
-trap 'exit_code=$?; kill -- $(jobs -p); exit $exit_code' SIGINT SIGTERM EXIT
 
 wait || exit 1 # fail if any child failed
