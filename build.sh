0241f178e49e0457468c7d60f81aa0e7a118d103
Jannis R <mail@jannisr.de>
2021-09-07 21:07:27 +0200

    build.sh: fix CSV download 🐛

--- build.sh
+++ build.sh
@@ -9,16 +9,16 @@ wget -r --no-parent --no-directories -P gtfs -N 'https://vbb-gtfs.jannisr.de/lat
 env | grep '^PG'
 
 NODE_ENV=production node_modules/.bin/gtfs-to-sql -d --trips-without-shape-id --routes-without-agency-id -- \
-	gtfs/agency.txt \
-	gtfs/calendar.txt \
-	gtfs/calendar_dates.txt \
-	gtfs/frequencies.txt \
-	gtfs/routes.txt \
-	gtfs/shapes.txt \
-	gtfs/stop_times.txt \
-	gtfs/stops.txt \
-	gtfs/transfers.txt \
-	gtfs/trips.txt \
+	gtfs/agency.csv \
+	gtfs/calendar.csv \
+	gtfs/calendar_dates.csv \
+	gtfs/frequencies.csv \
+	gtfs/routes.csv \
+	gtfs/shapes.csv \
+	gtfs/stop_times.csv \
+	gtfs/stops.csv \
+	gtfs/transfers.csv \
+	gtfs/trips.csv \
 	| psql -b
 
 lib="$(dirname $(realpath $0))/lib"
