a16b4bdd87d56dbe7a67b588e1f83de46a0ac7e6
Jannis R <mail@jannisr.de>
2021-11-20 22:48:55 +0100

    don't let hafas-client normalize stop names 🐛; 2.0.3
    see also bbnavi/gtfs-rt-feed@b8ff7e5

---
+++ test/hafas.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const {strictEqual} = require('assert')
+const hafas = require('../lib/hafas')
+
+const ctx = {
+	profile: hafas.profile,
+	common: {},
+	res: {},
+	opt: {},
+}
+const parsedHermannstr = hafas.profile.parseLocation(ctx, {
+	type: 'S',
+	lid: 'A=1@O=S+U Hermannstr. (Berlin)@X=13431430@Y=52467339@U=86@L=900079221@B=1@p=1637235113@',
+	name: 'S+U Hermannstr. (Berlin)',
+	extId: '900079221',
+	crd: {
+		x: 13431430,
+		y: 52467339,
+	},
+	pCls: 11,
+})
+strictEqual(
+	parsedHermannstr.name,
+	'S+U Hermannstr. (Berlin)',
+	'hafas.profile.parseLocation() should not normalize stop names',
+)
+
+console.info('HAFAS client seems to be configured properly ✔︎')
+process.exit()
--- test/index.sh
+++ test/index.sh
@@ -6,3 +6,4 @@ cd $(dirname $(realpath $0))
 set -x
 
 node truncate-at-in-seat-transfer.js
+node hafas.js
