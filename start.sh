8c40ec1b401b2c94624818ffa69d3a63091052e6
Jannis R <mail@jannisr.de>
2022-01-18 15:02:49 +0100

    expose feed URL

--- start.sh
+++ start.sh
@@ -20,6 +20,7 @@ NODE_ENV=production node_modules/.bin/match-with-gtfs \
 
 NODE_ENV=production node_modules/.bin/serve-as-gtfs-rt \
 	--signal-demand \
+	--static-feed-url 'https://vbb-gtfs.jannisr.de/latest/' \
 	&
 
 wait || exit 1 # fail if any child failed
