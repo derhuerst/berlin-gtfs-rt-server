381a900175a2980e001ff4cb4d82f2c2e6f13cab
Jannis R <mail@jannisr.de>
2024-01-06 16:01:46 +0100

    linter: re-add relaxed no-unused-vars rule

--- build-docs.js
+++ build-docs.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-// todo: use import assertions once they're supported by Node.js & ESLint
-// https://github.com/tc39/proposal-import-assertions
-import {createRequire} from 'module'
-const require = createRequire(import.meta.url)
-
 import {createReadStream, createWriteStream} from 'node:fs'
 import {copyFile} from 'node:fs/promises'
 import _technicalDocsCli from '@derhuerst/technical-docs-cli'
